1f02b8e15efbf550070c2ef9550a4cd639b61c64
Santiago Estrago <santiago.estrago@moove-it.com>
2015-10-27 13:17:39 -0300

    Initial commit

---
+++ src/articler/articler.module.js
@@ -0,0 +1,21 @@
+(function (angular) {
+
+  // Create all modules and define dependencies to make sure they exist
+  // and are loaded in the correct order to satisfy dependency injection
+  // before all nested files are concatenated by Gulp
+
+  // Config
+  angular.module('articler.config', [])
+      .value('articler.config', {
+          debug: true
+      });
+
+  // Modules
+  angular.module('articler.filters', ['articler']);
+  angular.module('articler',
+      [
+          'articler.config',
+          'articler.filters'
+      ]);
+
+})(angular);
